1071bce3e6a81449b1044def581ca63ac0afbb18
Andrej Vano <avano@redhat.com>
2016-02-16 20:05:04 +0100

    i3conf modded

--- i3conf.sh
+++ i3conf.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 BASE_DIR=`pwd`
 
-sudo apt-get install i3 i3blocks rofi compton lxappearance feh scrot imagemagick arandr xfce4-power-manager thunar git cpanminus libgtk2-perl libgtk3-perl geany
+sudo apt-get install i3 i3blocks rofi compton lxappearance feh scrot imagemagick arandr xfce4-power-manager thunar git cpanminus libgtk2-perl libgtk3-perl geany gsimplecal galculator
 #sudo apt-get purge dunst
 mkdir ~/.fonts
 
@@ -16,12 +16,13 @@ curl -L -o ssf.zip https://github.com/supermarin/YosemiteSanFranciscoFont/archiv
 # i3blocks contrib
 curl -L -o i3blockscontrib.zip https://github.com/vivien/i3blocks-contrib/archive/master.zip
 
-mkdir ~/.i3/scripts
+mkdir -p ~/.i3/scripts
 
 unzip awesome.zip
 unzip ssf.zip
 unzip sourcecodepro.zip
-unzip i3blockscontrib.zip -d ~/.i3/scripts
+unzip i3blockscontrib.zip 
+mv  i3blocks-contrib-master/* ~/.i3/scripts
 
 mv /tmp/font-awesome-4.5.0/fonts/fontawesome-webfont.ttf ~/.fonts
 mv /tmp/YosemiteSanFranciscoFont-master/*.ttf ~/.fonts
@@ -43,6 +44,7 @@ cpanm -n Linux::DesktopFiles --sudo
 cpanm -n Data::Dump --sudo
 git clone --depth 1 https://github.com/trizen/menutray
 cd menutray
+mv menutray ~/.i3
 mkdir ~/.config/menutray
 cp schema.pl ~/.config/menutray/
 
@@ -50,3 +52,5 @@ cp $BASE_DIR/config ~/.i3
 cp $BASE_DIR/i3blocks.conf ~/.i3
 cp $BASE_DIR/i3exit ~/.i3
 cp -r $BASE_DIR/scripts/* ~/.i3/scripts
+cp $BASE_DIR/.gtkrc-2.0 ~
+cp -r $BASE_DIR/.config ~
