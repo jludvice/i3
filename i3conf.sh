19110fe6df140ba4cc20e83ee64d1b85cc566197
Andrej Vano <avano@redhat.com>
2016-02-20 20:40:43 +0100

    i3 gaps + custom i3blocks

--- i3conf.sh
+++ i3conf.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 BASE_DIR=`pwd`
 
-sudo apt-get install i3 i3blocks rofi compton lxappearance feh scrot imagemagick arandr xfce4-power-manager thunar git cpanminus libgtk2-perl libgtk3-perl geany gsimplecal galculator
+# Moka icons
+
+sudo add-apt-repository ppa:moka/stable
+
+# Ambiance blackout flat theme
+sudo add-apt-repository ppa:ravefinity-project/ppa
+sudo apt-get update
+
+#Install i3 and i3blocks from https://github.com/Airblader/i3blocks-gaps https://github.com/Airblader/i3
+
+sudo apt-get install rofi compton lxappearance feh scrot imagemagick arandr xfce4-power-manager thunar git cpanminus libgtk2-perl libgtk3-perl geany gsimplecal galculator suckless-tools libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev sysstat moka-icon-theme ambiance-blackout-flat-colors ruby-ronn
 #sudo apt-get purge dunst
 mkdir ~/.fonts
 
@@ -13,7 +23,7 @@ curl -L -o awesome.zip https://fortawesome.github.io/Font-Awesome/assets/font-aw
 curl -L -o sourcecodepro.zip https://github.com/adobe-fonts/source-code-pro/archive/2.010R-ro/1.030R-it.zip
 curl -L -o ssf.zip https://github.com/supermarin/YosemiteSanFranciscoFont/archive/master.zip
 
-# i3blocks contrib
+# i3blocks contrib necessary?
 curl -L -o i3blockscontrib.zip https://github.com/vivien/i3blocks-contrib/archive/master.zip
 
 mkdir -p ~/.i3/scripts
@@ -28,16 +38,19 @@ mv /tmp/font-awesome-4.5.0/fonts/fontawesome-webfont.ttf ~/.fonts
 mv /tmp/YosemiteSanFranciscoFont-master/*.ttf ~/.fonts
 mv /tmp/source-code-pro-2.010R-ro-1.030R-it/TTF/*.ttf ~/.fonts
 
-# Moka icons
+# i3gaps
+git clone https://www.github.com/Airblader/i3 i3-gaps
+cd i3-gaps
+git checkout gaps && git pull
+make
+sudo make install
 
-sudo add-apt-repository ppa:moka/stable
-sudo apt-get update && sudo apt-get install moka-icon-theme
-
-# Ambiance blackout flat theme
-
-sudo add-apt-repository ppa:ravefinity-project/ppa
-sudo apt-get update
-sudo apt-get install ambiance-blackout-flat-colors
+# i3blocks-gaps
+cd /tmp
+git clone https://github.com/Airblader/i3blocks-gaps i3blocks-gaps
+cd i3blocks-gaps
+make clean all
+sudo make install
 
 # menutray
 cpanm -n Linux::DesktopFiles --sudo
@@ -48,9 +61,10 @@ mv menutray ~/.i3
 mkdir ~/.config/menutray
 cp schema.pl ~/.config/menutray/
 
-cp $BASE_DIR/config ~/.i3
-cp $BASE_DIR/i3blocks.conf ~/.i3
-cp $BASE_DIR/i3exit ~/.i3
+mkdir ~/.config/i3
+cp $BASE_DIR/config ~/.config/i3/
+cp $BASE_DIR/i3blocks.conf ~/.config/i3/
+cp $BASE_DIR/i3exit ~/.config/i3/
 cp -r $BASE_DIR/scripts/* ~/.i3/scripts
 cp $BASE_DIR/.gtkrc-2.0 ~
 cp -r $BASE_DIR/.config ~
