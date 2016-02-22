e395be65459fb7123fe3569fea3ce2a6d43677c5
name <you@exmaple.com>
2016-02-22 18:21:40 +0100

    memory script, i3blocks conf

--- date.sh
+++ date.sh
@@ -2,10 +2,10 @@
 #~/scripts/i3blocks/blocklets/date.sh
 
 case $BLOCK_BUTTON in
-  3)date '+%a %b %d, %Y' ;;
+  2)date '+%a %b %d, %Y' ;;
   1)gsimplecal ;;
-  2)galculator ;;
+  3)galculator ;;
 
 esac
 
-date '+%H:%M'
+date '+%d.%m.%Y %H:%M'
--- i3conf.sh
+++ i3conf.sh
@@ -9,13 +9,12 @@ sudo add-apt-repository ppa:moka/stable
 sudo add-apt-repository ppa:ravefinity-project/ppa
 
 # Arc theme
-echo 'deb http://download.opensuse.org/repositories/home:/Horst3180/xUbuntu_15.10/ /' >> /etc/apt/sources.list.d/arc-theme.list
+sudo sh -c "echo 'deb http://download.opensuse.org/repositories/home:/Horst3180/xUbuntu_15.10/ /' >> /etc/apt/sources.list.d/arc-theme.list"
 wget http://download.opensuse.org/repositories/home:Horst3180/xUbuntu_15.10/Release.key
 sudo apt-key add - < Release.key
 sudo apt-get update
-sudo apt-get install arc-theme
 
-sudo apt-get install rofi compton lxappearance feh scrot imagemagick arandr xfce4-power-manager thunar git cpanminus libgtk2-perl libgtk3-perl geany gsimplecal galculator suckless-tools libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev sysstat moka-icon-theme ambiance-blackout-flat-colors ruby-ronn
+sudo apt-get install rofi compton lxappearance feh scrot imagemagick arandr xfce4-power-manager thunar git cpanminus libgtk2-perl libgtk3-perl geany gsimplecal galculator suckless-tools libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev sysstat moka-icon-theme ambiance-blackout-flat-colors ruby-ronn arc-theme acpi
 
 cd /tmp
 
@@ -25,20 +24,16 @@ curl -L -o awesome.zip https://fortawesome.github.io/Font-Awesome/assets/font-aw
 curl -L -o sourcecodepro.zip https://github.com/adobe-fonts/source-code-pro/archive/2.010R-ro/1.030R-it.zip
 curl -L -o ssf.zip https://github.com/supermarin/YosemiteSanFranciscoFont/archive/master.zip
 
-# i3blocks contrib necessary?
-curl -L -o i3blockscontrib.zip https://github.com/vivien/i3blocks-contrib/archive/master.zip
-
-mkdir -p ~/.i3/scripts
-
 unzip awesome.zip
 unzip ssf.zip
 unzip sourcecodepro.zip
 unzip i3blockscontrib.zip 
-mv  i3blocks-contrib-master/* ~/.i3/scripts
 
-mv /tmp/font-awesome-4.5.0/fonts/fontawesome-webfont.ttf ~/.fonts
-mv /tmp/YosemiteSanFranciscoFont-master/*.ttf ~/.fonts
-mv /tmp/source-code-pro-2.010R-ro-1.030R-it/TTF/*.ttf ~/.fonts
+mkdir ~/.fonts
+
+mv /tmp/font-awesome-4.5.0/fonts/fontawesome-webfont.ttf ~/.fonts/
+mv /tmp/YosemiteSanFranciscoFont-master/*.ttf ~/.fonts/
+mv /tmp/source-code-pro-2.010R-ro-1.030R-it/TTF/*.ttf ~/.fonts/
 
 # i3gaps
 git clone https://www.github.com/Airblader/i3 i3-gaps
@@ -54,6 +49,8 @@ cd i3blocks-gaps
 make clean all
 sudo make install
 
+mkdir -pv ~/.config/i3
+
 # menutray
 cpanm -n Linux::DesktopFiles --sudo
 cpanm -n Data::Dump --sudo
@@ -63,10 +60,9 @@ mv menutray ~/.config/i3
 mkdir ~/.config/menutray
 cp schema.pl ~/.config/menutray/
 
-mkdir ~/.config/i3
 cp $BASE_DIR/config ~/.config/i3/
 cp $BASE_DIR/i3blocks.conf ~/.config/i3/
 cp $BASE_DIR/i3exit ~/.config/i3/
-cp -r $BASE_DIR/scripts/* ~/.i3/scripts
+cp $BASE_DIR/*.sh ~/.config/i3/
 cp $BASE_DIR/.gtkrc-2.0 ~
 cp -r $BASE_DIR/.config ~
